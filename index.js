5ca5f08b02b2b698a8bc5e409a5699c2041709bf
parth-html <76615401+parth-html@users.noreply.github.com>
2021-05-17 16:06:49 +0530

    last update

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
 const TelegramBot = require("node-telegram-bot-api");
 const http = require("http");
 const request = require("request");
+require("dotenv").config();
 
-const token = "";
+const token = process.env.TOKEN;
 
 const bot = new TelegramBot(token, { polling: false });
 
-// bot.on("message", (msg) => {
-//   console.log(msg);
-//type other code here
+var pincode = "384315";
+var intval = 60000;
 
-var dt = new Date();
-var dt1 = `${dt.getDate()}-${dt.getMonth() + 1}-${dt.getFullYear()}`;
-
-var requestLoop = setInterval(function () {
+var loopfun = function () {
+  var dt = new Date();
+  var dt1 = `${dt.getDate()}-${dt.getMonth() + 1}-${dt.getFullYear()}`;
   request(
     {
-      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=384330&date=24-05-2021`,
+      url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=${pincode}&date=${dt1}`,
       method: "GET",
       timeout: 10000,
       followRedirect: true,
@@ -25,44 +24,43 @@ var requestLoop = setInterval(function () {
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         body = JSON.parse(body);
-        // console.log(body);
-        if (body.centers) {
+        if (body.centers.length) {
           var text =
             "hello everyone, new vaccine schedule available here is the details :\n\n";
           body.centers.forEach((center) => {
-            let cntr = center.name;
-            text += `center name :${cntr}\nAddress :${center.address}\n`;
-
+            var temp = true;
             center.sessions.forEach((session) => {
-              text += `\ndate :${session.date}\ndose 1 :${session.available_capacity_dose1}\ndose 2 :${session.available_capacity_dose2}\nmin age :${session.min_age_limit} \n------------------`;
+              if (
+                session.available_capacity_dose1 ||
+                session.available_capacity_dose2
+              ) {
+                if (temp) {
+                  text += `center name :${center.name}\nAddress :${center.address}\n`;
+                  temp = false;
+                }
+                text += `\ndate :${session.date}\ndose 1 :${session.available_capacity_dose1}\ndose 2 :${session.available_capacity_dose2}\nmin age :${session.min_age_limit} \n------------------`;
+              }
             });
-            // if (center.sessions[0].available_capacity_dose1) {
-            //   console.log(center.name);
-            // }
-            text += "\n=====================================\n";
+            if (!temp) {
+              text += "\n==================\n";
+            }
           });
-          console.log(text);
-          // console.log("sucess!");
-          // console.log(body.sessions[0].available_capacity_dose1);
-          // const chatId = "@vaccineReminder";
-          // var text =
-          // "Available dose :" + body.sessions[0].available_capacity_dose1;
-          // bot.sendMessage(chatId, text);
+          const chatId = process.env.CHATID;
+          bot.sendMessage(chatId, text);
+          intval = 3600000;
+          clearInterval(requestLoop);
+          requestLoop = setInterval(loopfun, intval);
         } else {
-          console.log("error!");
+          if (intval == 3600000) {
+            clearInterval(requestLoop);
+            intval = 60000;
+            requestLoop = setInterval(loopfun, intval);
+          }
         }
-        // console.log(response);
       } else {
         console.log("error" + response.statusCode);
       }
     }
   );
-}, 10000);
-
-// const chatId = "@vaccineReminder";
-// var text = 1;
-// setInterval(() => {
-//   bot.sendMessage(chatId, "Received your message: " + text);
-//   console.log(text);
-//   text++;
-// }, 1000);
+};
+var requestLoop = setInterval(loopfun, intval);
